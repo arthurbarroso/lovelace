7160cee2863ff5bf822e911fcd06899c298c8d48
arthurbarroso <arthurdsb_@hotmail.com>
2021-05-15 15:41:25 -0300

    feat(users): better error messages/propagation

--- src/lovelace/users/requests.clj
+++ src/lovelace/users/requests.clj
@@ -1,12 +1,12 @@
 (ns lovelace.users.requests
-  (:require [clj-http.client :as http]
-            [cheshire.core :as json]
-            [lovelace.utils :refer [make-request]]))
+  (:require
+   [cheshire.core :as json]
+   [lovelace.utils :refer [make-request safe-get]]))
 
 (defn fetch-user
   "Makes a GET request to Notion's API. Takes `token` and `id` as parameters."
   ([token id]
-   (http/get
+   (safe-get
     (str "https://api.notion.com/v1/users/" id)
     (make-request token))))
 
@@ -14,15 +14,15 @@
   "Makes a GET request to Notion's API. Takes `token` as a parameter. Is also able to take
   the optional parameters `page-size` and `start-cursor`"
   ([token]
-   (http/get
+   (safe-get
     "https://api.notion.com/v1/users/"
     (make-request token)))
   ([token page-size]
-   (http/get
+   (safe-get
     (str "https://api.notion.com/v1/users" "?page_size=" page-size)
     (make-request token)))
   ([token page-size start-cursor]
-   (http/get
+   (safe-get
     (str "https://api.notion.com/v1/users" "?page_size=" page-size "&start_cursor" start-cursor)
     (make-request token))))
 
@@ -30,13 +30,25 @@
   "Retrieves a list of users of a given workspace. Takes the authentication token as a parameter.
   Is also able to take the optional parameters `page-size` and `start-cursor`"
   ([token]
-   (json/parse-string (:body (fetch-users token)) true))
+   (let [response (fetch-users token)]
+     (if (:error response)
+       response
+       (json/parse-string (:body response) true))))
   ([token page-size]
-   (json/parse-string (:body (fetch-users token page-size)) true))
+   (let [response (fetch-users token page-size)]
+     (if (:error response)
+       response
+       (json/parse-string (:body response) true))))
   ([token page-size start-cursor]
-   (json/parse-string (:body (fetch-users token page-size start-cursor)) true)))
+   (let [response (fetch-users token page-size start-cursor)]
+     (if (:error response)
+       response
+       (json/parse-string (:body response) true)))))
 
 (defn get-single-user
   "Retrieves a single users of a given workspace. Takes the authentication token and the user's id as parameters."
   [token id]
-  (json/parse-string (:body (fetch-user token id)) true))
+  (let [response (fetch-user token id)]
+    (if (:error response)
+      response
+      (json/parse-string (:body response) true))))
