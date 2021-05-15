752b6b756a5a0e9c655f8d49bc9439519732870c
arthurbarroso <arthurdsb_@hotmail.com>
2021-05-15 14:34:02 -0300

    feat(databases): better error messages

--- src/lovelace/databases/requests.clj
+++ src/lovelace/databases/requests.clj
@@ -4,11 +4,25 @@
             [lovelace.databases.specs :refer [validate-db-query]]
             [lovelace.utils :refer [make-request]]))
 
+(defn safe-get [url req]
+  (try
+    (http/get
+     url
+     req)
+    (catch Exception e {:error (.getMessage e)})))
+
+(defn safe-post [url req]
+  (try
+    (http/post
+     url
+     req)
+    (catch Exception e {:error (.getMessage e)})))
+
 (defn fetch-database
   "Makes a GET request to Notion's database API and retrieves the data from a database.
   Takes the authentication token and the database's id as parameters"
   [token id]
-  (http/get
+  (safe-get
    (str "https://api.notion.com/v1/databases/" id)
    (make-request token)))
 
@@ -16,22 +30,25 @@
   "Retrieves data from a database based off of it's unique id.
   Takes the authentication token and the database's id as parameters"
   [token id]
-  (json/parse-string (:body (fetch-database token id)) true))
+  (let [response (fetch-database token id)]
+    (if (:error response)
+      response
+      (json/parse-string (:body response) true))))
 
 (defn post-database
   "Makes a POST request to Notion's database API and searches data within a certain database.
   Takes the authentication token, the database's id and the query to search for data within the database.
   Is also able to take the optional parameters `page-size` and `start-cursor` for pagination"
   ([token id data]
-   (http/post
+   (safe-post
     (str "https://api.notion.com/v1/databases/" id "/query")
     (make-request token data)))
   ([token id data page-size]
-   (http/post
+   (safe-post
     (str "https://api.notion.com/v1/databases/" id "/query" "?page_size=" page-size)
     (make-request token data)))
   ([token id data page-size start-cursor]
-   (http/post
+   (safe-post
     (str "https://api.notion.com/v1/databases/" id "/query" "?page_size=" page-size "&start_cursor=" start-cursor)
     (make-request token data))))
 
@@ -40,13 +57,22 @@
   Is also able to take the optional parameters `page-size` and `start-cursor`"
   ([token id query]
    (if (validate-db-query query)
-     (json/parse-string (:body (post-database token id (json/encode query))) true)
+     (let [response (post-database token id (json/encode query))]
+       (if (:error response)
+         response
+         (json/parse-string (:body response) true)))
      {:error "query doesn't match the query spec"}))
   ([token id query page-size]
    (if (validate-db-query query)
-     (json/parse-string (:body (post-database token id (json/encode query) page-size)) true)
+     (let [response (post-database token id (json/encode query) page-size)]
+       (if (:error response)
+         response
+         (json/parse-string (:body response) true)))
      {:error "query doesn't match the query spec"}))
   ([token id query page-size start-cursor]
    (if (validate-db-query query)
-     (json/parse-string (:body (post-database token id (json/encode query) page-size start-cursor)) true)
+     (let [response (:body (post-database token id (json/encode query) page-size start-cursor))]
+       (if (:error response)
+         response
+         (json/parse-string (:body response) true)))
      {:error "query doesn't match the query spec"})))
