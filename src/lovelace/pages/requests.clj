65c09d50c07c8181d8deb8c23763e0eded2fd800
arthurbarroso <arthurdsb_@hotmail.com>
2021-05-15 15:37:23 -0300

    feat(pages): better error messages/propagation

--- src/lovelace/pages/requests.clj
+++ src/lovelace/pages/requests.clj
@@ -1,27 +1,30 @@
 (ns lovelace.pages.requests
-  (:require [clj-http.client :as http]
-            [cheshire.core :as json]
-            [lovelace.pages.specs :refer [validate-page-creation]]
-            [lovelace.utils :refer [make-request]]))
+  (:require
+   [cheshire.core :as json]
+   [lovelace.pages.specs :refer [validate-page-creation]]
+   [lovelace.utils :refer [make-request safe-get safe-post safe-patch]]))
 
 (defn get-page
   "Makes a GET request to Notion's API to retrieve data from a page.
   Takes in the authentication token and the page id as parameters"
   [token id]
-  (http/get
+  (safe-get
    (str "https://api.notion.com/v1/pages/" id)
    (make-request token)))
 
 (defn retrieve-page
   "Retrieves a page's data from Notion. Takes the authentication token and the page's unique id as parameters"
   [token id]
-  (json/parse-string (:body (get-page token id)) true))
+  (let [response (get-page token id)]
+    (if (:error response)
+      response
+      (json/parse-string (:body response) true))))
 
 (defn post-page
   "Makes a POST request to Notion's API in order to create a new page.
   Takes the authentication token and the new page's content as parameters"
   [token data]
-  (http/post
+  (safe-post
    "https://api.notion.com/v1/pages/"
    (make-request token data)))
 
@@ -30,14 +33,17 @@
   Takes the authentication token and the new page's content/body as parameters"
   [token body]
   (if (validate-page-creation body)
-    (json/parse-string (:body (post-page token (json/encode body))) true)
+    (let [response (post-page token (json/encode body))]
+      (if (:error response)
+        response
+        (json/parse-string (:body response) true)))
     {:error "page-body doesn't match the page spec"}))
 
 (defn patch-page
   "Makes a PATCH request to Notion's API in order to change a page's properties.
   Takes the authentication token, the page's id, and the new page's properties as parameters"
   [token id data]
-  (http/patch
+  (safe-patch
    (str "https://api.notion.com/v1/pages/" id)
    (make-request token data)))
 
@@ -45,4 +51,7 @@
   "Update a page's properties based off of it's unique id.
   Takes the authentication token, a page's id and a page's new properties as parameters"
   [token id body]
-  (json/parse-string (:body (patch-page token id (json/encode body))) true))
+  (let [response (patch-page token id (json/encode body))]
+    (if (:error response)
+      response
+      (json/parse-string (:body response) true))))
